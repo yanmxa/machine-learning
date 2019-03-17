125aa468517857906e757668d19d584d1af2c462
Yanmeng <ymeng125@126.com>
2019-03-17 11:25:25 +0800

    add pca

--- gradient/simulation.py
+++ gradient/simulation.py
@@ -24,6 +24,16 @@ def gradient_descent(initial_theta, eta, n_iters=1e4, epsilon=1e-8):
         i_iters += 1
     return theta_history
 
+def dJ_debug(theta, X_b, y, epsilon=0.01):
+    res = np.empty(len(theta))
+    for i in range(len(theta)):
+        theta_1 = theta.copy()
+        theta_1[i] += epsilon
+        theta_2 = theta.copy()
+        theta_2[i] -= epsilon
+        res[i] = (J(theta_1, X_b, y) - J(theta_2, X_b, y)) / (2 * epsilon)
+    return res
+
 def plot_theta_history(plot_x, theta_history):
     plt.plot(plot_x, J(plot_x))
     plt.plot(np.array(theta_history), J(np.array(theta_history)), color='r', marker='+')
